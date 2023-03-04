8493b2f35aba1904378a3bb4d86739a8816a2102
Ulyanov Nikita <nandi.saber@yandex.ru>
2023-03-05 02:21:10 +0300

    ---

---
+++ Oznokom praktika/task_A183.py
@@ -0,0 +1,37 @@
+# Напишите функцию sum_cube(n, m), которая будет вычислять сумму кубов чисел в заданном диапазоне, 
+# начиная с меньшего (но не включая его) до большего (включая). Первый аргумент не обязательно должен быть большим числом.
+# Если оба числа совпадают, тогда диапазон пуст и результат должен быть 0. 
+#
+# Примеры:
+# sum_cube(2,3) => 27 -> 3^3 = 27
+# sum_cube(3,2) => 27 -> 3^3 = 27
+# sum_cube(0,4) => 100 -> 1^3+2^3+3^3+4^3 = 100
+# sum_cube(17, 14) => 12384 -> 15^3+16^3+17^3 = 12384
+
+import traceback
+
+def sum_cube(n, m):
+    sum = 0
+    if n < m:
+        for i in range(n+1,m+1):
+            sum += i ** 3
+    elif n > m:
+        for i in range(m+1,n+1):
+            sum += i ** 3
+    else:
+        return 0
+    return sum
+
+# Тесты
+try:
+    assert sum_cube(2,3) == 27
+    assert sum_cube(3,2) == 27
+    assert sum_cube(0,4) == 100
+    assert sum_cube(17, 14) == 12384
+    assert sum_cube(9, 9) == 0
+    assert sum_cube(5, 0) == 225
+except AssertionError:
+    print("TEST ERROR")
+    traceback.print_exc()
+else:
+    print("TEST PASSED")
---
+++ Oznokom praktika/task_A579.py
@@ -0,0 +1,69 @@
+# Создать список (каталог мобильных приложений), состоящий из словарей (приложение). Словари должны содержать как минимум 5 полей
+# (например, номер, название, рейтинг...). В список добавить хотя бы 10 словарей.
+# Конструкция вида:
+# apps = [{"id" : 123456, "title" : "Google Play", "rating" : 4.9,...} , {...}, {...}, ...]
+# Реализовать функции:
+# – вывода информации о всех приложениях;
+# – вывода информации о приложении по введенному с клавиатуры номеру;
+# – вывода количества приложений, с оценкой выше введённого;
+# – обновлении всей информации о приложении по введенному номеру;
+# – удалении приложения по номеру.
+# Провести тестирование функций.
+
+def getinfo(apps): # – вывода информации о всех приложениях;
+    for i in range(len(apps)):
+        print (apps[i])
+
+def getinfo_about(apps, num): # – вывода информации о приложении по введенному с клавиатуры номеру;
+    print(apps[num - 1])
+
+def rating(apps, rate): # – вывода количества приложений, с оценкой выше введённого
+    counter = 0
+    for i in apps:
+        if i["rating"] >= rate:
+            counter += 1
+    print(counter)
+
+def update(apps,num): # – обновлении всей информации о приложении по введенному номеру;
+    apps[num]["id"] = input("enter id: ")
+    apps[num]["title"] = input("enter title: ")
+    apps[num]["description"] = input("enter description: ")
+    apps[num]["rating"] = input("enter rating: ")
+    apps[num]["last Feedback"] = input("enter last feedback: ")
+    getinfo(apps)
+    return apps
+
+def erase(apps, num): # – удалении приложения по номеру.
+    del apps[num - 1]
+    getinfo(apps)
+    return apps
+
+apps = [{"id" : 1 , "title" : "Google Play", "description" : "Applications for downloading other applications" ,"rating" : 4.9 , "last Feedback" : "Good app" },
+        {"id" : 2 , "title" : "Microsoft 365 (Office)", "description": "One application for documents, tables, presentations, PDF and other files. ", "rating" : 4.8, "last Feedback" : "Comfortable!"},
+        {"id" : 3 , "title" : "Yandex translator", "description" : "Applications for tranlating texts and photos" ,"rating" : 4.5 , "last Feedback" : "I like the app" },
+        {"id" : 4 , "title" : "Super Auto Pats", "description" : "Gather the strongest team of animals and fight with your friends" ,"rating" : 4.2 , "last feedback" : "The game freezes" },
+        {"id" : 5 , "title" : "Subway Surfers", "description" : "Help Jake escape from Inspector and his dog" ,"rating" : 4.5 , "last Feedback" : "Not for me" },
+        {"id" : 6 , "title" : "Twitter", "description" : "Find out what's happening in the world right now" ,"rating" : 3.0 , "last Feedback" : "Poor app" },
+        {"id" : 7 , "title" : "Instagram", "description" : "communicate in the language of photos" ,"rating" : 3.0 , "last Feedback" : "After the update, app lags" },
+        {"id" : 8 , "title" : "CapCut", "description" : "Edit videos with music and effects" ,"rating" : 4.3 , "last Feedback" : "There are few effects , but this is a amazing application" },
+        {"id" : 9 , "title" : "Aviasales", "description" : "Search for cheap tickets in the CIS" ,"rating" : 4.6 , "last Feedback" : "A hotel? Trivago!" },
+        {"id" : 10 , "title" : "Discord", "description" : "Connect with people all over the world" ,"rating" : 4.5 , "last Feedback" : "I always use it" },]
+
+
+getinfo(apps) #вывода информации о всех приложениях;+
+
+print("enter number") #вывода информации о приложении по введенному с клавиатуры номеру;
+getinfo_about(apps, int(input()))
+
+print("enter rating") # – вывода количества приложений, с оценкой выше введённого;
+rating(apps, int(input())) 
+
+print("enter number") # – обновлении всей информации о приложении по введенному номеру;
+num = int(input())-1
+update(apps,num)
+
+print("enter number") # – удалении приложения по номеру.
+apps = erase(apps, int(input()))
+
+        
+
